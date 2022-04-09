bd934d5836bc71a8ca2119244cf5e0376bd4d0c9
Wanderson <lopez.wanderson@gmail.com>
2022-04-09 17:09:43 -0600

    Initial Commit

---
+++ FindUserLocation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FindUserLocation
+//
+//  Created by Wanderson López on 9/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FindUserLocation/FindUserLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindUserLocationApp.swift
+//  FindUserLocation
+//
+//  Created by Wanderson López on 9/04/22.
+//
+
+import SwiftUI
+
+@main
+struct FindUserLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
