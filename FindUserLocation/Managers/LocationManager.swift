4793ab4888c672d4400aa0d34b1afeee0da4a736
Wanderson <lopez.wanderson@gmail.com>
2022-04-09 17:18:47 -0600

    Se creó la clase LocationManager

--- FindUserLocation/Managers/LocationManager.swift
+++ FindUserLocation/Managers/LocationManager.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject {
+    
+    private let locationManager = CLLocationManager()
+    @Published var location: CLLocation?
+    
+    override init() {
+        super.init()
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        
+        DispatchQueue.main.async {
+            self.location = location
+        }
+    }
+}
