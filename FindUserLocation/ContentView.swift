3d28d11ca7e997d7184cd603f721f15bcb636ab3
Wanderson <lopez.wanderson@gmail.com>
2022-04-09 17:43:43 -0600

    Implementación del extension en la vista

--- FindUserLocation/ContentView.swift
+++ FindUserLocation/ContentView.swift
@@ -7,14 +7,31 @@
 
 import SwiftUI
 import MapKit
+import Combine
 
 struct ContentView: View {
     @ObservedObject private var locationManager = LocationManager()
     @State private var region = MKCoordinateRegion.defaultRegion
+    @State private var cancellable: AnyCancellable?
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            if locationManager.location != nil {
+                Map(coordinateRegion: $region, interactionModes: .all, showsUserLocation: true, userTrackingMode: nil)
+            }
+            else {
+                Text("Locating user location...")
+            }
+        }
+        .onAppear{
+            setCurrentLocation()
+        }
+    }
+    
+    private func setCurrentLocation() {
+        cancellable = locationManager.$location.sink { location in
+            region = MKCoordinateRegion(center: location?.coordinate ?? CLLocationCoordinate2D(), latitudinalMeters: 500, longitudinalMeters: 500)
+        }
     }
 }
 
